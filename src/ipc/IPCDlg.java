7b779121c3b023c441e418f8cad112eac63874bf
JoonheeJeong <jeonggoo75@gmail.com>
2019-03-24 17:15:55 +0900

    Add comments and structure of actionPerformed method in IPCDlg.java

--- src/ipc/IPCDlg.java
+++ src/ipc/IPCDlg.java
@@ -148,14 +148,55 @@ public class IPCDlg extends JFrame implements BaseLayer {
 
 	class setAddressListener implements ActionListener {
 		@Override
+		/*
+		 * Setting Button이 눌렸을 때의 동작 처리
+		 * 1. SrcAddress의 text를 ChatAppLayer header에 저장
+		 * 2. DstAddress의 text를 ChatAppLayer header에 저장
+		 * 3. SrcAddress의 text를 SocketLayer Server port에 저장
+		 * 4. DstAddress의 text를 SocketLayer Client port에 저장
+		 * 5. SocketLayer의 서버를 실행시킴(SocketLayer Thread 동작)
+		 * 6. “Setting” Button을 “Reset” Button으로 변경
+		 * 7. SrcAddress, DstAddress의 값 변경 못하게 설정
+		 * 8. “Reset” Button을 누를 시 SrcAddress, DstAddress의text를 공백으로 변경
+		 * 9. “Reset” Button - > “Setting” Button
+		 * 
+		 * Send Button이 눌렸을 때의 농작 처리
+		 * 1. Setting Button이 “Reset”인지 확인
+		 * 2. ChattingWrite에 적은 Text를 ChattingArea에 보여준다.
+		 * 3. ChatAppLayer에 Send()호출해서 String을 Byte형식으로변경해서 보낸다.
+		 * 4. 주소 값이 없으면 “주소 설정 오류” MessageDialog를띄운다.
+		 * */
 		public void actionPerformed(ActionEvent e) {
-			/*
-			 * 
-			 * 과제 Setting 버튼과 Send 버튼을 누를 시 행동
-			 * 
-			 * Setting 버튼 누를 시 SocketLayer에서 포트 설정
-			 * 
-			 */
+			if (e.getSource() == Setting_Button) {
+				
+				if (Setting_Button.getText() == "Reset") {
+					srcAddress.setText("");
+					dstAddress.setText("");
+					Setting_Button.setText("Setting");
+				} else {
+					String Ssrc = srcAddress.getText();
+					String Sdst = dstAddress.getText();
+					
+					int src = Integer.parseInt(Ssrc);
+					int dst = Integer.parseInt(Sdst);
+					
+					((SocketLayer) m_LayerMgr.GetLayer("Socket")).setServerPort(src);
+					((SocketLayer) m_LayerMgr.GetLayer("Socket")).setClientPort(dst);
+					
+					((ChatAppLayer) m_LayerMgr.GetLayer("Chat")).SetEnetSrcAddress(src);
+					((ChatAppLayer) m_LayerMgr.GetLayer("Chat")).SetEnetDstAddress(dst);
+					((SocketLayer) m_LayerMgr.GetLayer("Socket")).Receive();
+					
+					Setting_Button.setText("Reset");
+					srcAddress.setEditable(false);
+					dstAddress.setEditable(false);
+				}
+				
+			}
+			
+			if (e.getSource() == Chat_send_Button) {
+				
+			}
 		}
 	}
 
